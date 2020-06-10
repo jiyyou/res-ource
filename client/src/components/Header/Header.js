4d1ff879c8d6f3e20483214f7cf8eac608868560
Jiyo <ji.yyou@gmail.com>
2020-06-10 17:42:41 -0400

    common scss updated

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-function Header() {
-	return (
-		<h1>Header</h1>
-	)
+class Header extends React.Component {
+	state = {
+		loggedIn: false
+	}
+
+	// componentDidMount() {
+	// 	const authToken = sessionStorage.getItem('authToken');
+		
+
+	// }
+
+	render() {
+		return (
+			<header>
+				<h1>TEST</h1>
+			</header>
+		)
+	}
 }
 
 export default Header;
---
+++ client/src/components/PostCard/PostCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PostCard(props) {
+	return (
+		<div className="postCard">
+
+		</div>
+	)
+}
+
+export default PostCard;
