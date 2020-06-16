d16907fa5b30c31f81e938a9698114a567471fb8
Jiyo <ji.yyou@gmail.com>
2020-06-15 21:42:05 -0400

    post post, post sub

--- server/routes/subRoutes.js
+++ server/routes/subRoutes.js
@@ -10,8 +10,18 @@ router
 			.fetchAll()
 			.then(subs => {
 				res.status(200).json(subs);
-			})
+			});
 	})
+	.post((req, res) => {
+		new Sub({
+			name: req.body.name,
+			description: req.body.description
+		})
+			.save()
+			.then(newSub => {
+				res.status(201).json(newSub);
+			});
+	});
 
 router
 	.route('/:id')
@@ -20,7 +30,22 @@ router
 			.fetchAll({ withRelated: ['posts']})
 			.then(sub => {
 				res.status(200).json(sub);
-			})
+			});
 	})
-	
+	.post((req, res) => {
+		new Post({
+			title: req.body.title,
+			content: req.body.content,
+			sub_id: req.params.id
+		})
+			.save()
+			.then(newPost => {
+				console.log(newPost);
+				res.status(201).json(newPost);
+			})
+			.catch(err => {
+				console.log(err);
+			})
+	});
+
 module.exports = router;
