0d83fb3f1cd656ef73b683d8750ad7ccfef2417f
Jiyo <ji.yyou@gmail.com>
2020-06-29 17:23:26 -0400

    migration seed added to production knexfile

--- server/knexfile.js
+++ server/knexfile.js
@@ -14,5 +14,11 @@ module.exports = {
 	production: {
 		client: 'mysql',
 		connection: process.env.JAWSDB_URL
+	},
+	migrations: {
+		directory: __dirname + '/migrations'
+	},
+	seeds: {
+		directory: __dirname + '/seeds'
 	}
 };
