4c4b30bdbd98acd0371b80cd0b97089b855f32f4
Jiyo <ji.yyou@gmail.com>
2020-07-01 17:58:07 -0400

    testing

--- server/server.js
+++ server/server.js
@@ -46,9 +46,10 @@ if (process.env.JAWSDB_URL) {
 }
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('../client/build'))
+	const root = path.join(__dirname, '../client', 'build');
+	app.use(express.static(root));
 	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+		res.sendFile('index.html', { root });
 	})
 }
 
