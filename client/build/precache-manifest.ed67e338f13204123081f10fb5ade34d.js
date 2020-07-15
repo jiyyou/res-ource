05aec631627e7c080595b58bdf6a6d815460ceeb
Jiyo <ji.yyou@gmail.com>
2020-07-15 16:35:39 -0400

    build updated

---
+++ client/build/precache-manifest.ed67e338f13204123081f10fb5ade34d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f02dabc1589039a641ae970bc9a85f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f94d4410ea1580295425",
+    "url": "/static/css/main.d22156a4.chunk.css"
+  },
+  {
+    "revision": "dcbc44b68c7d99606056",
+    "url": "/static/js/2.1b1fcac1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1b1fcac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f94d4410ea1580295425",
+    "url": "/static/js/main.d67753d8.chunk.js"
+  },
+  {
+    "revision": "349b16348608356f3cc0",
+    "url": "/static/js/runtime-main.ee5eeb27.js"
+  },
+  {
+    "revision": "286069001111ea3ed233483011ce0c09",
+    "url": "/static/media/Icon-search.28606900.svg"
+  },
+  {
+    "revision": "7fcc2b46832290d7928e03690a7f69b6",
+    "url": "/static/media/RES-ource.7fcc2b46.png"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "5b44818d2b9eda3e23cd5edd7b49b7d5",
+    "url": "/static/media/Roboto-BoldItalic.5b44818d.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "a3ce4440f2abf76f4a1b14b83920138c",
+    "url": "/static/media/Roboto-LightItalic.a3ce4440.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "cf23e1bb619029496260760b72aebd30",
+    "url": "/static/media/Roboto-MediumItalic.cf23e1bb.ttf"
+  }
+]);
